98fc7cf4174237d2165df67817092cff2f4fd69c
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-11-13 16:34:20 +0700

    Commit file .h .c "software_timer"

--- CodeSTM32/Buttons_Switches/Core/Inc/software_timer.h
+++ CodeSTM32/Buttons_Switches/Core/Inc/software_timer.h
@@ -8,6 +8,14 @@
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
+#define NUM_OF_TIMERS		4
 
+extern int TIME_CYCLE;
+extern int timer_counter[NUM_OF_TIMERS];
+extern int timer_flag[NUM_OF_TIMERS];
+
+void setTimer(int index, int duration);
+void clearTimer(int index);
+void timerRun();
 
 #endif /* INC_SOFTWARE_TIMER_H_ */

d5a2ff285b1a77ec5d746b4f7d4c82d716c30b4f
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-11-13 15:00:59 +0700

    Commit file .h .c "global"

--- CodeSTM32/Buttons_Switches/Core/Inc/global.h
+++ CodeSTM32/Buttons_Switches/Core/Inc/global.h
@@ -8,6 +8,28 @@
 #ifndef INC_GLOBAL_H_
 #define INC_GLOBAL_H_
 
+#define INIT		0
+#define MODE_1		1
+#define MODE_2		2
+#define MODE_3		3
+#define MODE_4		4
 
+#define RED1_GREEN2       10
+#define RED1_YELLOW2      11
+#define GREEN1_RED2       12
+#define YELLOW1_RED2      13
+
+#define	AUTO_RED		21
+#define AUTO_YELLOW		22
+#define AUOTO_GREEN		23
+
+#define INC_RED			31
+#define INC_YELLOW		32
+#define INC_GREEN		33
+
+extern int status;
+extern int PERIOD_RED;
+extern int PERIOD_YELLOW;
+extern int PERIOD_GREEN;
 
 #endif /* INC_GLOBAL_H_ */
--- CodeSTM32/Buttons_Switches/Core/Src/global.c
+++ CodeSTM32/Buttons_Switches/Core/Src/global.c
@@ -6,3 +6,9 @@
  */
 
 #include "global.h"
+
+int status = INIT;
+
+int PERIOD_RED = 5;
+int PERIOD_YELLOW = 2;
+int PERIOD_GREEN = 3;

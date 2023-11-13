aa83e92559aac5ff74371c73a60e0f3e321c319f
Vy Nguyen <tuyetvynguyen18@gmail.com>
2023-11-13 17:37:37 +0700

    Commit file .h .c "button"

--- CodeSTM32/Buttons_Switches/Core/Inc/button.h
+++ CodeSTM32/Buttons_Switches/Core/Inc/button.h
@@ -8,6 +8,16 @@
 #ifndef INC_BUTTON_H_
 #define INC_BUTTON_H_
 
+#include "main.h"
+#include "software_timer.h"
 
+#define NORMAL_STATE	SET
+#define PRESSED_STATE	RESET
+
+#define NUM_OF_BUTTONS	3
+
+int isButtonPressed(int index);
+void subKeyProcess(int index);
+void getKeyInput();
 
 #endif /* INC_BUTTON_H_ */
